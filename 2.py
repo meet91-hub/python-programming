4c5b7857488f46a8b24ecc1a9ab52e31a13b9f6d
meet91-hub <meet.marvaniya127721@marwadiuniversity.ac.in>
2025-09-07 20:24:33 +0530

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep  7 18:58:46 2025
+Created on Sun Sep  7 18:54:43 2025
 
 @author: Meet
 """
 
-num=int(input("enter the number:="))
-if num%2==0:
-    print("even")
-else:
-    print("odd")
+print("a    a*2    a*3")
+for a in range(1, 5):
+    print(f"{a:<5}{a**2:<7}{a**3}")
